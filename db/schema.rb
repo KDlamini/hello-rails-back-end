e5a0c911def9be2a6969ee0485f2617b79f10032
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-04-13 12:53:39 +0200

    Add db:seed with Greeting data

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_13_104348) do
   enable_extension "plpgsql"
 
   create_table "greetings", force: :cascade do |t|
-    t.string "greetings"
+    t.string "greeting"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
