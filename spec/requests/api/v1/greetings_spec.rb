93bfde485b5aa870bfcbc7067c5164490dac9427
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-04-13 14:58:07 +0200

    Correct rubocop offenses

--- spec/requests/api/v1/greetings_spec.rb
+++ spec/requests/api/v1/greetings_spec.rb
@@ -1,12 +1,9 @@
 require 'swagger_helper'
 
 RSpec.describe 'api/v1/greetings', type: :request do
-
   path '/api/v1/greetings' do
-
     get('list greetings') do
       response(200, 'successful') do
-
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
